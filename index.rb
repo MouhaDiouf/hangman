3c0a222a095fb2f93b6383f914aeeb18e8f97f91
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2021-11-12 21:41:32 +0000

    Add game saving functionality

--- index.rb
+++ index.rb
@@ -1,3 +1,5 @@
+require 'tty-prompt'
+require 'json'
 
 class Game 
     @@file = File.read('dictionary.txt').split
@@ -19,44 +21,88 @@ class Game
         @user_guess = Array.new(@@secret.length).fill('_')
     end 
 
+
+    def save_game 
+        game_state = { 
+             secret: @@secret, 
+             guesses_left: @guesses, 
+             user_guess: @user_guess, 
+             played: @played 
+         }.to_json 
+         Dir.mkdir('saved') unless Dir.exist?('saved')
+         filename = "saved/game_#{Time.now.to_s}.json"
+         File.open(filename, 'w') do |file|
+            file.puts game_state
+          end
+
+     end 
+  
+
+     def load_game
+         
+     end 
+
     def start 
         choose_word
         puts "Welcome to the hangman game! You have #{@guesses} chances to guess the secret word chosen by the Computer"
         puts  '_' * @@secret.length
         until @user_guess.join == @@secret || @guesses == 0
-            puts "Make a guess (one letter at a time) from a-z (uppercased included)"
-            letter_input = gets.chomp
-            if @played.include? letter_input
-                puts "This is already played. Try again"
-                next
-            elsif  @@secret.include?(letter_input)
-                @@secret.split('').each_with_index do |chr, idx|
-                    if letter_input == chr 
-                        @user_guess[idx] = chr 
+            prompt = TTY::Prompt.new
+            greeting = 'Do you like to keep playing or reload a previous game?'
+            choices = ['Keep Playing' ,'Save & Quit']
+            answer = prompt.select(greeting, choices)
+            if answer == choices[0]
+                puts "Make a guess (one letter at a time) from a-z (uppercased included)"
+                letter_input = gets.chomp
+                if @played.include? letter_input
+                    puts "This is already played. Try again"
+                    next
+                elsif  @@secret.include?(letter_input)
+                    @@secret.split('').each_with_index do |chr, idx|
+                        if letter_input == chr 
+                            @user_guess[idx] = chr 
+                        end 
                     end 
-                end 
-            end
-            @guesses -= 1
-            puts "#{@guesses} guesses left"
-            @played << letter_input 
-            puts @user_guess.join
+                end
+                @guesses -= 1
+                puts "#{@guesses} guesses left"
+                @played << letter_input 
+                puts @user_guess.join
+            else
+                save_game
+                exit
+            end 
+
         end
-        
         check_game_over
         
-    end 
+        
+    end #end of start method 
 
+  
     def check_game_over 
         if @guesses > 0 
             puts "You win! 🎉🎉"
         else
             puts "Game over :(. It ways #{@@secret}" 
         end 
+        exit
     end 
+
+  
     
 end 
 
 
+prompt = TTY::Prompt.new
+greeting = 'Do you like to play a new game or reload a previous one?'
+choices = ['New Game' ,'Previous One']
+answer = prompt.select(greeting, choices)
+if answer == choices[0]
+    game = Game.new 
+    game.start
+else
 
-game = Game.new 
-game.start
+
+end 
+
