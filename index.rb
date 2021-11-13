d5d171a3f1138b2f010f37f0f15c584089dfa162
Mouhamadou Diouf <mouhamadoudioufsn@gmail.com>
2021-11-13 00:02:07 +0000

    Create the save and load logic

--- index.rb
+++ index.rb
@@ -2,51 +2,51 @@ require 'tty-prompt'
 require 'json'
 
 class Game 
-    @@file = File.read('dictionary.txt').split
-    def initialize 
+        @@file = File.read('dictionary.txt').split
         @@secret =  nil 
-        @guesses =  nil 
-        @user_guess = nil 
-        @played = []
-    end
+        @@guesses =  nil 
+        @@user_guess = nil 
+        @@played = []
+        @@current_file = nil 
 
-    def choose_word 
+    def self.choose_word 
         file = File.read('dictionary.txt').split
         @@secret = file[rand(0...file.length)]
-        @guesses = @@secret.length + 3
+        @@guesses = @@secret.length + 3
         
         while (@@secret.length < 5 || @@secret.length > 12)
             @@secret = file[rand(0...file.length)]
         end 
-        @user_guess = Array.new(@@secret.length).fill('_')
+        @@user_guess = Array.new(@@secret.length).fill('_')
     end 
 
+    def self.delete_previous
+        File.delete(@@current_file) if !@@current_file.nil? && File.exist?(@@current_file)
+    end 
 
-    def save_game 
+    def self.save_game 
         game_state = { 
              secret: @@secret, 
-             guesses_left: @guesses, 
-             user_guess: @user_guess, 
-             played: @played 
+             guesses_left: @@guesses, 
+             user_guess: @@user_guess, 
+             played: @@played 
          }.to_json 
          Dir.mkdir('saved') unless Dir.exist?('saved')
-         filename = "saved/game_#{Time.now.to_s}.json"
+         filename = "saved/game_#{Time.new.strftime("%d%m%Y_%k%M%S") }.json"
          File.open(filename, 'w') do |file|
             file.puts game_state
           end
 
+          delete_previous
+
      end 
   
+    def self.start 
+        choose_word if @@secret.nil?
 
-     def load_game
-         
-     end 
-
-    def start 
-        choose_word
-        puts "Welcome to the hangman game! You have #{@guesses} chances to guess the secret word chosen by the Computer"
-        puts  '_' * @@secret.length
-        until @user_guess.join == @@secret || @guesses == 0
+        puts "Welcome to the hangman game! You have #{@@guesses} chances to guess the secret word chosen by the Computer"
+        puts @@user_guess.join
+        until @@user_guess.join == @@secret || @@guesses == 0
             prompt = TTY::Prompt.new
             greeting = 'Do you like to keep playing or reload a previous game?'
             choices = ['Keep Playing' ,'Save & Quit']
@@ -54,20 +54,20 @@ class Game
             if answer == choices[0]
                 puts "Make a guess (one letter at a time) from a-z (uppercased included)"
                 letter_input = gets.chomp
-                if @played.include? letter_input
+                if @@played.include? letter_input
                     puts "This is already played. Try again"
                     next
                 elsif  @@secret.include?(letter_input)
                     @@secret.split('').each_with_index do |chr, idx|
                         if letter_input == chr 
-                            @user_guess[idx] = chr 
+                            @@user_guess[idx] = chr 
                         end 
                     end 
                 end
-                @guesses -= 1
-                puts "#{@guesses} guesses left"
-                @played << letter_input 
-                puts @user_guess.join
+                @@guesses -= 1
+                puts "#{@@guesses} guesses left"
+                @@played << letter_input 
+                puts @@user_guess.join
             else
                 save_game
                 exit
@@ -79,30 +79,47 @@ class Game
         
     end #end of start method 
 
+    def self.rename_previous
+        File.rename(@@current_file, "game_#{Time.new.strftime("%d%m%Y_%k%M%S")}.json")
+    end 
+
   
-    def check_game_over 
-        if @guesses > 0 
+    def self.check_game_over 
+        if @@guesses > 0 
             puts "You win! 🎉🎉"
         else
             puts "Game over :(. It ways #{@@secret}" 
         end 
+            rename_previous
         exit
     end 
 
-  
+  def self.load_game(data, selected_file)
+    @@secret = data['secret']
+    @@guesses = data['guesses_left']
+    @@user_guess = data['user_guess']
+    @@played = data['played'] || []
+    @@current_file = selected_file
+
+
+    start 
+
+  end 
     
 end 
-
 
 prompt = TTY::Prompt.new
 greeting = 'Do you like to play a new game or reload a previous one?'
 choices = ['New Game' ,'Previous One']
 answer = prompt.select(greeting, choices)
 if answer == choices[0]
-    game = Game.new 
-    game.start
+    Game.start
 else
-
-
+   previous_files = Dir["./saved/*.json"].reverse
+   message = "Please select a version"
+   selected_file = prompt.select(message, previous_files)
+    file = File.read(selected_file)
+    data_hash = JSON.parse(file)
+    Game.load_game(data_hash, selected_file)
 end 
 
